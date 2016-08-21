21d99abc3f378c1217025ae5c607a119a75303e7
Olena Deering <olena.ivashyna@gmail.com>
2016-08-21 12:18:09 -0600

    add second view to mainPane

--- apps/todos_two/resources/main_page.js
+++ apps/todos_two/resources/main_page.js
@@ -11,13 +11,21 @@ TodosTwo.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page
   // load.
   mainPane: SC.MainPane.design({
-    childViews: ['labelView'],
+    childViews: ['labelView', 'labelView2'],
 
     labelView: SC.LabelView.design({
       classNames: ['welcome-label'],
       layout: { centerX: 0, centerY: 0, width: 300, height: 24 },
       tagName: "h1",
       value: "Welcome to SproutCore from Olena!"
+    }),
+
+    labelView2: SC.LabelView.design({
+      classNames: ['welcome-label'],
+      layout: { centerX: 0, centerY: 50, width: 200, height: 24 },
+      //textAlign: SC.ALIGN_CENTER,
+      tagName: "h1",
+      value: "Second View"
     })
   })
 
