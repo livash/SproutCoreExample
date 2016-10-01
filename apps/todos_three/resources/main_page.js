9dbf1339cadb403ceb1f2baf61f088e338e6444a
Olena Deering <olena.ivashyna@gmail.com>
2016-10-01 15:37:31 -0600

    add main_page view content; add todo_item content

--- apps/todos_three/resources/main_page.js
+++ apps/todos_three/resources/main_page.js
@@ -1,32 +1,69 @@
-// ==========================================================================
-// Project:   TodosThree - mainPage
-// Copyright: @2016 My Company, Inc.
-// ==========================================================================
-/*globals TodosThree */
-
-// This page describes the main user interface for your application.
 TodosThree.mainPage = SC.Page.design({
-  field: Sc.outlet('mainPane.newTodoField.field'),
+  field: SC.outlet('mainPane.newTodoField.field'),
 
-  mainPane: Sc.MainPane.design({
-    childViews: {},
+  mainPane: SC.MainPane.design({
+    childViews: ['header', 'newTodoField', 'todosList', 'footer'],
     defaultResponder: 'TodosThree.statechart',
 
-    header: {
-      
-    },
+    header: SC.ToolbarView.design({
+      layout: { centerX: 0, width: 500, top 0, height: 36 },
+      childViews: ['title', 'completeAll'],
+      completeAll: SC.CheckboxView.design(SC.AutoResize, {
+        aitoResizPadding: { width: 47 },
+        title: 'Mark all as done',
+        valueBinding: 'TodosThree.completedTodosController.areAllCompleted'
+      }),
+      title: SC.LabelView.design({
+        layout: { left: 0, right: 0, top 0, bottom: 0 },
+        totalTodosBinding: SC.Binding.oneWay('TodosThree.todosController.length'),
+        completedTodosBinding: SC.Binding.oneWay('TodosThree.completedTodosController.length'),
+        value: function() {
+          return 'TodosThree (' + (this.get('totalTodos') - this.get('completedTodos')) + ')';
+        }.property('totalTodos', 'completedTodos').cacheable()
+      })
+    }),
 
-    newTodoField: {
-      
-    },
+    newTodoField: SC.View.design({
+      classNames: ['new-todo'],
+      layout: { centerX: 0, width: 500, top: 36, height: 36 },
+      childViews: ['field', 'button'],
+      field: SC.TextFieldView.design({
+        hint: 'What needs to be done?'
+      }),
+      button: SC.ButtonView.design({
+        controlSize: SC.HUGE_CONTROL_SIZE,
+        layout: { centerY; 0, height: 30, height: 12, zIndex: 100 },
+        title: 'Add',
+        action: 'addTodo',
+        valueBinding: '.page.field.value',
+        isDefaultBinding: '.page.field.focused'
+      })
+    }),
 
-    todosList: {
-      
-    },
+    todosList: SC.ScrollView.design({
+      layout: { centerX: 0, width: 500, top: 72, bottom: 36 },
+      contentView: SC.ListView.design({
+        contentBinding: SC.Binding.ondeWay('TodosThree.todosController'),
+        rowHeight: 36,
+        exampleView: SC.CheckboxView.design({
+          classNames: ['todo-item'],
+          valueBinding: '.content.isCompleted',
+          titleBinding: SC.Binding.oneWay('.content.title')
+        })
+      })
+    }),
 
-    footer: {
-      
-    }
+    footer: SC.Toolbar.View.design({
+      layout: { centerX: 0, width: 500, bottom: 0, height: 36 },
+      childViews: ['clearCompletedTodos'],
+      clearCompletedTodos: SC.ButtonView.design(SC.AutoResize, {
+        controlSize: SC.HUGE_CONTROL_SIZE,
+        layout: { centerY: 0, height: 30, right: 12, zIndex: 100 },
+        isEnabledBinding: SC.Binding.oneWay('TodosThree.completedTodosController.length'),
+        title: 'Clear completed todos',
+        action: 'clearCompletedTodos'
+      })
+    })
   })
 
 });
